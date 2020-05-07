0888837c95e7c09e7e889f287238fdeeea98bdb1
Tirth Hihoriya <tirth.hihoriya@gmail.com>
2020-05-08 00:40:24 +0530

    js

--- app.js
+++ app.js
@@ -31,9 +31,7 @@ function computeResults(e) {
 //   document.getElementById("totalInterest").innerHTML = "%" + totalInterest;
 
   document.getElementById("totalPayment").innerHTML = "Rs. " + total;
-  if(whatsappnumber == null){
-    document.location = "https://wa.me/+919825108232?text=Totallllll is  Rs. " + total;
-  }
+  
   document.location = "https://wa.me/+91" + whatsappnumber + "?text=Totallllll is  Rs. " + total;
   
   
