5644827164a876994c38e5707526386523fba5e3
Tirth Hihoriya <tirth.hihoriya@gmail.com>
2020-05-08 01:37:45 +0530

    js

--- app.js
+++ app.js
@@ -22,11 +22,19 @@ function computeResults(e) {
   //Compute Total Payment
 
   const total = (har*200 + dj*200 + mar*280 + kmar*360 + jee*220 + gm*0.8 + sm*0.6 + pbm*0.6 + cm*0.3).toFixed(2);
-  const msg = "PRAPANNA NUTRI FOODS  %0a %0a ";
+  var msg = "PRAPANNA NUTRI FOODS  %0a %0a ";
 msg = msg + "Items      Qtn   Price %0a";
 if(har != "")
 {
   msg = msg + "Hardal(200/kg)   "+ har+  "kg    " +  har*200;        
+}
+if(dj != "")
+{
+  msg = msg + "DhanaJeeru(200/kg)   "+ gj+  "kg    " +  dj*200;        
+}
+if(mar != "")
+{
+  msg = msg + "Marchu(280/kg)   "+ mar+  "kg    " +  mar*280;        
 }
   
   //Show results
@@ -34,10 +42,10 @@ if(har != "")
 //   document.getElementById("monthlyPayment").innerHTML = "$" + monthlyPayment;
 
 //   document.getElementById("totalInterest").innerHTML = "%" + totalInterest;
-console.log(whatsappnumber)
+console.log(msg)
   document.getElementById("totalPayment").innerHTML = "Rs. " + total;
   
-  document.location = "https://wa.me/+91" + whatsappnumber + "?text="+ msg;
+  // document.location = "https://wa.me/+91" + whatsappnumber + "?text="+ msg;
   
   
   e.preventDefault();
