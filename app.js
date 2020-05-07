52ee11a24213d840340c1d1969547feabb224c31
Tirth Hihoriya <tirth.hihoriya@gmail.com>
2020-05-08 01:59:19 +0530

    doing much more

--- app.js
+++ app.js
@@ -22,21 +22,54 @@ function computeResults(e) {
   //Compute Total Payment
 
   const total = (har*200 + dj*200 + mar*280 + kmar*360 + jee*220 + gm*0.8 + sm*0.6 + pbm*0.6 + cm*0.3).toFixed(2);
-  var msg = "PRAPANNA NUTRI FOODS  %0a %0a ";
-msg = msg + "Items      Qtn   Price %0a";
+  var msg = "  %0a      PRAPANNA NUTRI FOODS  %0a %0a ";
+msg = msg + "    Items                Qtn     Price(Rs.) %0a";
 if(har != "")
 {
-  msg = msg + "Hardal(200/kg)   "+ har+  "kg    " +  har*200;        
+  msg = msg + "Hardal (200/kg)   "+ har+  "kg    " +  har*200 + " %0a ";        
 }
 if(dj != "")
 {
-  msg = msg + "DhanaJeeru(200/kg)   "+ dj+  "kg    " +  dj*200;        
+  msg = msg + "Dhana Jeeru (200/kg)   "+ dj+  "kg    " +  dj*200 + " %0a ";        
 }
 if(mar != "")
 {
-  msg = msg + "Marchu(280/kg)   "+ mar+  "kg    " +  mar*280;        
+  msg = msg + "Marchu (280/kg)   "+ mar+  "kg    " +  mar*280 + " %0a ";        
 }
   
+if(kmar != "")
+{
+  msg = msg + "Kashmiri Marchu (360/kg) "+ kmar+  "kg    " +  kmar*360 + " %0a ";        
+}
+
+if(jee != "")
+{
+  msg = msg + "Jeeru (220/kg)   "+ jee+  "kg    " +  jee*220 + " %0a ";        
+}
+
+if(gm != "")
+{
+  msg = msg + "Garam Masala (40/50gm)   "+ gm+  "g    " +  gm*0.8 + " %0a ";        
+}
+if(sm != "")
+{
+  msg = msg + "Sambhar Masala (30/50gm)   "+ sm+  "g    " +  sm*0.6 + " %0a ";        
+}
+if(pbm != "")
+{
+  msg = msg + "Pav bhaji Masala (30/50gm)   "+ pbm+  "g    " +  pbm*0.6 + " %0a ";        
+}
+if(cm != "")
+{
+  msg = msg + "Chat Masala (30/100g)   "+ cm+  "g    " +  cm*0.3 + " %0a ";    
+    
+}
+
+msg = msg + " %0a Total Amount is                       " + total;
+
+msg = msg + " %0a %0a  Happy Life = Healthy Life ";
+
+
   //Show results
 
 //   document.getElementById("monthlyPayment").innerHTML = "$" + monthlyPayment;
@@ -45,7 +78,8 @@ if(mar != "")
 console.log(msg)
   document.getElementById("totalPayment").innerHTML = "Rs. " + total;
   
-  document.location = "https://wa.me/+91" + whatsappnumber + "?text="+ msg;
+  // document.location = "https://wa.me/+91" + whatsappnumber + "?text="+ msg;
+  document.location = "https://wa.me/+919825108232?text="+ msg;
   
   
   e.preventDefault();
