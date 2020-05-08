88815406ed753dd9ce430e55f0315d97dcae7b00
Tirth Hihoriya <tirth.hihoriya@gmail.com>
2020-05-08 09:48:28 +0530

    vc

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ document.getElementById("loan-form").addEventListener("submit", computeResults);
 function computeResults(e) {
   // UI
 
-  const har = document.getElementById("har").value;
-  const dj = document.getElementById("dj").value;
-  const mar = document.getElementById("mar").value;
-  const kmar = document.getElementById("kmar").value;
-  const jee = document.getElementById("jee").value;
-  const gm = document.getElementById("gm").value;
-  const sm = document.getElementById("sm").value;
-  const pbm = document.getElementById("pbm").value;
-  const cm = document.getElementById("cm").value;
+  const har = 2//document.getElementById("har").value;
+  const dj = 3//document.getElementById("dj").value;
+  const mar = 1//document.getElementById("mar").value;
+  const kmar = 5//document.getElementById("kmar").value;
+  const jee = 3//document.getElementById("jee").value;
+  const gm = 50//document.getElementById("gm").value;
+  const sm = 100//document.getElementById("sm").value;
+  const pbm = 200//document.getElementById("pbm").value;
+  const cm = 250//document.getElementById("cm").value;
   const whatsappnumber = document.getElementById("whatsappnumber").value;
  
 
@@ -22,17 +22,17 @@ function computeResults(e) {
   //Compute Total Payment
 
   const total = (har*200 + dj*200 + mar*280 + kmar*360 + jee*220 + gm*0.8 + sm*0.6 + pbm*0.6 + cm*0.3).toFixed(2);
-  var msg = ".  %0a      PRAPANNA NUTRI FOODS  %0a ";
-  msg = msg + "__________________________________ %0a  %0a";
+  var msg = " %0a______________________________ %0a      PRAPANNA NUTRI FOODS  %0a ";
+  msg = msg + "______________________________ %0a  %0a";
 msg = msg + " Items               Qtn    Price(Rs.) %0a";
-msg = msg + "------------------------------------ %0a ";
+msg = msg + "----------------------------------------- %0a ";
 if(har != "")
 {
   msg = msg + "Hardal              "+ har+  "kg    " +  har*200 + " %0a ";        
 }
 if(dj != "")
 {
-  msg = msg + "Dhana Jeeru       "+ dj+  "kg    " +  dj*200 + " %0a ";        
+  msg = msg + "Dhana Jeeru     "+ dj+  "kg    " +  dj*200 + " %0a ";        
 }
 if(mar != "")
 {
@@ -41,7 +41,7 @@ if(mar != "")
   
 if(kmar != "")
 {
-  msg = msg + "Kashmiri Marchu   "+ kmar+  "kg    " +  kmar*360 + " %0a ";        
+  msg = msg + "Kashmiri      "+ kmar+  "kg    " +  kmar*360 + " %0a ";        
 }
 
 if(jee != "")
@@ -51,24 +51,24 @@ if(jee != "")
 
 if(gm != "")
 {
-  msg = msg + "Garam Masala      "+ gm+  "g    " +  gm*0.8 + " %0a ";        
+  msg = msg + "Garam M.    "+ gm+  "g    " +  gm*0.8 + " %0a ";        
 }
 if(sm != "")
 {
-  msg = msg + "Sambhar Masala   "+ sm+  "g    " +  sm*0.6 + " %0a ";        
+  msg = msg + "Sambhar M."+ sm+  "g    " +  sm*0.6 + " %0a ";        
 }
 if(pbm != "")
 {
-  msg = msg + "Pavbhaji Masala   "+ pbm+  "g    " +  pbm*0.6 + " %0a ";        
+  msg = msg + "Pavbhaji M."+ pbm+  "g    " +  pbm*0.6 + " %0a ";        
 }
 if(cm != "")
 {
-  msg = msg + "Chat Masala       "+ cm+  "g    " +  cm*0.3 + " %0a ";    
+  msg = msg + "Chat M.   "+ cm+  "g    " +  cm*0.3 + " %0a ";    
     
 }
-msg = msg + "------------------------------------ %0a ";
-msg = msg + " %0a Total Amount is            Rs. " + total;
-msg = msg + "------------------------------------ %0a ";
+msg = msg + "----------------------------------------- %0a ";
+msg = msg + " %0a Total Amount =  Rs. " + total;
+msg = msg + "----------------------------------------- %0a ";
 msg = msg + " %0a %0a  Happy Life = Healthy Life ";
 
 
@@ -82,7 +82,7 @@ console.log(msg)
   
   // document.location = "https://wa.me/+91" + whatsappnumber + "?text="+ msg;
   
-  document.location = "https://wa.me/+919825108232?text="+ msg;
+  document.location = "https://wa.me/+919852127105?text="+ msg;
   
   
   e.preventDefault();
